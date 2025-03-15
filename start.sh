0291c42b32a3274db3db126fafed7f62aff5393c
max-carlo <max.carlo@mailbox.org>
2025-03-15 21:32:43 +0100

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pip install -r requirements.txt
+# WebDriver für Selenium automatisch installieren
+python -c "from selenium import webdriver; from webdriver_manager.chrome import ChromeDriverManager; webdriver.Chrome(ChromeDriverManager().install())"
+# Starte Streamlit App
+streamlit run app.py --server.port=10000 --server.address=0.0.0.0

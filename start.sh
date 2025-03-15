a287c4c4c8957a4f3dfc58d04c234dc60f9b44c8
max-carlo <max.carlo@mailbox.org>
2025-03-15 21:40:46 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ pip install -r requirements.txt
 # WebDriver für Selenium automatisch installieren
 python -c "from selenium import webdriver; from webdriver_manager.chrome import ChromeDriverManager; webdriver.Chrome(ChromeDriverManager().install())"
 # Starte Streamlit App
-streamlit run app.py --server.port=10000 --server.address=0.0.0.0
+streamlit run momentum.py --server.port=10000 --server.address=0.0.0.0
